48c31df929c842df7818edfdb1630f0fc1460f60
jiong-long <jiong_long@163.com>
2021-08-05 19:10:15 +0800

    rabbitMQ

---
+++ provider/src/main/java/com/jianghu/mq/rabbit/Producer.java
@@ -0,0 +1,36 @@
+package com.jianghu.mq.rabbit;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @description: rabbitMQ 生产者
+ * @author: OF3848
+ * @create: 2021-08-05 18:47
+ */
+public class Producer {
+
+    private final static String QUEUE_NAME = "queue_name";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        //设置 RabbitMQ 地址
+        factory.setHost("localhost");
+        //建立到代理服务器到连接
+        Connection conn = factory.newConnection();
+        //获得信道
+        Channel channel = conn.createChannel();
+        //声明交换器
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        String message = "Hello World!";
+        channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+        System.out.println("消息发送成功");
+    }
+}
